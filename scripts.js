b7635ad9b7abb02e691d07dca3fa3a84150f2de5
Nathan Minchow <nathan.minchow@outlook.com>
2018-09-17 00:34:31 -0500

    Redo mobile layout

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
-var header = document.getElementById("left-column-container");
-var title = document.getElementById("title");
-var links = document.getElementById("links");
-var projects = document.getElementById("projects");
-var contact = document.getElementById("contact");
-var elements = [title, links, projects, contact];
-var colors = [
-  "rgba(42, 45, 52, 1)",
-  "rgba(0, 157, 220, 1)",
-  "rgba(242, 100, 48, 1)",
-  "rgba(0, 155, 114, 1)"
-];
+// var header = document.getElementById("left-column-container");
+// var title = document.getElementById("title");
+// var links = document.getElementById("links");
+// var projects = document.getElementById("projects");
+// var contact = document.getElementById("contact");
+// var elements = [title, links, projects, contact];
+// var colors = [
+//   "rgba(42, 45, 52, 1)",
+//   "rgba(0, 157, 220, 1)",
+//   "rgba(242, 100, 48, 1)",
+//   "rgba(0, 155, 114, 1)"
+// ];
 
-function setBackgroundTransition(element, color) {
-  element.addEventListener("mouseenter", function(event) {
-    header.style.backgroundColor = color;
-  });
+// function setBackgroundTransition(element, color) {
+//   element.addEventListener("mouseenter", function(event) {
+//     header.style.backgroundColor = color;
+//   });
 
-  element.addEventListener("mouseleave", function(event) {
-    header.style.backgroundColor = "rgba(255, 255, 255, 0)";
-  });
-}
+//   element.addEventListener("mouseleave", function(event) {
+//     header.style.backgroundColor = "rgba(255, 255, 255, 0)";
+//   });
+// }
 
-for (i = 0; i < elements.length; i++) {
-  setBackgroundTransition(elements[i], colors[i]);
-}
+// for (i = 0; i < elements.length; i++) {
+//   setBackgroundTransition(elements[i], colors[i]);
+// }
